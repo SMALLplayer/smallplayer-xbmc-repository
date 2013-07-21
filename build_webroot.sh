6a15608ec2b341dc20a7615315cc2221b1a8a412
Rico van Genugten <rico@amplexus.vps.codeventures.net>
2013-07-21 21:39:32 +0200

    Reconfigured repository, hosted externally now.

---
+++ build_webroot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo Creating repository.smallplayer.zip
+mkdir web
+rm web/repository.smallplayer.zip
+zip web/repository.smallplayer.zip repository.smallplayer -r -X
+
+echo Creating skin.smallplayer-0.0.1.zip
+mkdir web/skin.smallplayer
+cd repo
+zip ../web/skin.smallplayer/skin.smallplayer-0.0.1.zip skin.smallplayer -r -X
+python ../addons_xml_generator.py
+mv addons.* ../web/
+cd ..
--- update_repo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-zip repository.smallplayer.zip repository.smallplayer
-cd repo
-python ../addons_xml_generator.py
-cd ..
